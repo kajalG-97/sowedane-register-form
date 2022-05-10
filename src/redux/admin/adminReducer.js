98e3243d45467bf9dbd8fb257dca947548ae1401
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-05-10 15:50:49 +0530

    session stringify

--- src/redux/admin/adminReducer.js
+++ src/redux/admin/adminReducer.js
@@ -18,6 +18,7 @@ export const AdminReducer = (store = initialState, { type, payload }) => {
         case aLOGIN_SUCCESS: return { loding: false, error: false, isAAuthenticated: true, token: payload.token, admin: payload };
 
         case LOGOUT: return { ...initialState }
+        // localStorage. removeItem('myData')
 
         default: return store;
     }
